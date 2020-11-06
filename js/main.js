421a0afc493d3b92b96040e3aa9f9b6c5fddf8ab
Iliya <inc4rochka@gmail.com>
2020-11-06 10:27:54 +0300

    Second commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//Фиксированное меню
+
+const fixMenu = document.querySelector('.fixed-menu');
+
+window.addEventListener('scroll', function(){
+    if(this.pageYOffset > 10){
+        fixMenu.classList.add('active');
+    }
+    else{
+        fixMenu.classList.remove('active');
+    }
+})
+
+
+//Динамичный плейсхолдер
+
+const formInputs = document.querySelectorAll('.form-field');
+for (let item of formInputs) {
+    const thisPlaceholder = item.nextElementSibling;
+
+    item.addEventListener('focus', function (){
+        thisPlaceholder.classList.add('active');
+    });
+
+    item.addEventListener('blur', function(){
+        if (item.value == '') {
+            thisPlaceholder.classList.remove('active');
+        }
+    })
+}
